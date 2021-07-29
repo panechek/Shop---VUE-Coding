ec99e215399c195bc965a83964df7f7c05117605
panechek <panechek@inbox.ru>
2021-07-29 17:41:00 +0300

    cart component

---
+++ components/cart-comp.js
@@ -0,0 +1,6 @@
+const cartTemp = `<div><a style="padding-right: 0;" href="cart.html" class="header__cart"><img src="img/cart.svg"
+alt="cart"></a></div>`
+
+Vue.component('cart-comp', {
+    template: cartTemp
+})
