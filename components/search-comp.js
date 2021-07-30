411f8b05f811244e2a36cea0be6d9398a0bd52fc
panechek <panechek@inbox.ru>
2021-07-31 00:10:36 +0300

    search has done

--- components/search-comp.js
+++ components/search-comp.js
@@ -1,21 +1,36 @@
-const searchTemp = `<div><button  class="header__search">
+const searchTemp = `<div>
+<button v-on:click="seeSearch" class="header__search">
 <img src="img/search.png" alt="search"></button>
-       <input type="text" class="goods-search">
-       <button class="search-button" type="button">Искать</button></div>`
-
+<template v-if="isVisibleSearch">
+<input type="text" v-model="searchText" class="goods-search">
+<button v-on:click="filtred" class="search-button">Искать</button>
+</template>
+</div>`
 
 Vue.component('search-comp', {
     template: searchTemp,
-    searchLine: '',
-    isVisibleSearch: false,
+    data() {
+        return {
+            isVisibleSearch: false,
+            searchText: ""
+        }
+    },
+
+
     methods: {
         seeSearch() {
-            this.isVisibleSearch = !this.isVisibleSearch
+            this.isVisibleSearch = !this.isVisibleSearch;
+            console.log(this.isVisibleSearch)
         },
+        // filtred() {
+        //     const regex = new RegExp(this.searchLine);
+        //     this.filteredGoods = this.goods.filter((good) => regex.test(good.title))
 
+        // },
         filtred() {
-            const regex = new RegExp(this.searchLine);
-            this.filteredGoods = this.goods.filter((good) => regex.test(good.title))
-        },
+            this.$emit('filtred', this.searchText)
+            console.log(this.searchText)
+        }
     }
+
 })
--- main.js
+++ main.js
@@ -8,19 +8,24 @@
          newPrice: 0,
          cart: [],
          id: '',
+
      },
 
      methods: {
 
-         filtred() {
-             const regex = new RegExp(this.searchLine);
-             this.filteredGoods = this.goods.filter((good) => regex.test(good.title))
+         filtred(searchText) {
+             console.log(searchText);
+             const regex = new RegExp(searchText);
+             this.filteredGoods = this.goods.filter((good) => regex.test(good.title));
+             console.log(this.filteredGoods)
          },
 
          addToCart(e) {
              this.cart.push(this.goods[e.target.id - 101]);
              console.log(this.cart)
          },
+
+
 
 
 
